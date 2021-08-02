1d8d7f0a01ea9540d5f1ec366bfed28d587ba8af
uk2459644 <uk2459644@gmail.com>
2021-08-02 11:48:15 +0530

    row aws data

--- backend/settings.py
+++ backend/settings.py
@@ -186,9 +186,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #aws settings
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = 'AKIAS5W5G43XB4TIDHLP'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'institute-env'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
@@ -196,7 +196,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_LOCATION = 'static'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
+AWS_S3_REGION_NAME = 'ap-south-1'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
